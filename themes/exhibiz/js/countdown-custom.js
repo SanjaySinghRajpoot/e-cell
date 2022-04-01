0b57701e265dd6bd7d36e4b7ec1573574f4c7805
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-04-01 20:12:06 +0530

    files

---
+++ themes/exhibiz/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 5, 20, 8)}); // year, month, date, hour
+        });
+});		
+
